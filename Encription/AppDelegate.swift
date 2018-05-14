1fa9fec74bae3fa1cfbcafd1d003473dfa1c5fa2
Britto Thomas <britto.thomas@uvionicstech.com>
2018-05-14 15:29:23 +0530

    first commit

--- Encription/AppDelegate.swift
+++ Encription/AppDelegate.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import CryptoSwift
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    static var encryptionKey = "Abcdefghijklmnop"
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //Encription
+        //Dictionary > JSON Data > String ASCII > Data UTF8 > Encripted Data
+        let json = ["key1":"value1","ket2":"value2"]
+        let data = try! JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+        let string = String(data: data, encoding: String.Encoding(rawValue: String.Encoding.ascii.rawValue))!
+        let stringData = string.data(using: .utf8)!
+        let encrypted = try! AES(key: AppDelegate.encryptionKey.bytes, blockMode: .ECB, padding: .pkcs7).encrypt([UInt8](stringData))
+        let encryptedData = Data(encrypted)
+
+        
+        //Decription
+        //Encripted Data > Decripted Data > String ASCII > JSON Data > Dictionary
+        let decrypted = try! AES(key: AppDelegate.encryptionKey.bytes, blockMode: .ECB, padding: .pkcs7).decrypt([UInt8](encryptedData))
+        let decryptedStringData = Data(decrypted)
+        let decryptedString = String(bytes: decryptedStringData.bytes, encoding: .utf8) ?? "Could not decrypt"
+        let decryptedData = decryptedString.data(using:String.Encoding(rawValue: String.Encoding.ascii.rawValue))!
+        let decryptedJson = try! JSONSerialization.jsonObject(with: decryptedData, options: JSONSerialization.ReadingOptions.allowFragments)
+        print(decryptedJson)
+
+
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-Encription/Pods-Encription-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Encription : NSObject
+@end
+@implementation PodsDummy_Pods_Encription
+@end
